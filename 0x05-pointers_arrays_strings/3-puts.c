807bc0b7b0f55466579d8c643f0ed5633e7fc4dc
oussama <om7.oussama@gmail.com>
2023-03-27 06:52:37 -0700

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,12 @@
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
-void _puts(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-}
-
+void _puts(char *str) 
+  {
+     while (*str != '\0' ) 
+ {
+    _putchar(*str);
+        str++;
+} 
+     _putchar('\n'); 
+  } 
