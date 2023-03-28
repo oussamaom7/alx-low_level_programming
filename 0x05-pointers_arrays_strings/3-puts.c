06c58c27bb45efbb413fd45684d54ab04edd5ed9
oussama <om7.oussama@gmail.com>
2023-03-27 19:13:17 -0700

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
- include "main.h"
+es (13 sloc)  209 Bytes
 
-  /** 
-   * _puts - prints string
-   * 
-   * @str: input string 
-   * Return: no return
-   */
-   void _puts(char *str) 
-  {
-     while (*str != '\0' ) 
- {
-    _putchar(*str);
-        str++;
-} 
-     _putchar('\n'); 
-  } 
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
+
